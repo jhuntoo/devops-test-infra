01bd7ba28c41efb109441356513b91e9fa141531
jhuntoo <jonnolee84@gmai.com>
2017-06-26 12:25:01 +0100

    add install_kops scripts.sh

---
+++ scripts/install_kops.sh
@@ -0,0 +1,6 @@
+KOPS_VERSION=1.6.2
+PLATFORM=`uname | tr '[:upper:]' '[:lower:]'`
+
+mkdir -p .bin
+[ -f ./.bin/kops ] && echo 'skipping download of kops' || /usr/bin/curl -L -o ./.bin/kops https://github.com/kubernetes/kops/releases/download/$KOPS_VERSION/kops-$PLATFORM-amd64
+chmod +x ./.bin/kops
