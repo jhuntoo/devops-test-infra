2f6cae77fa04ea8073c0013ed4c023c4c9f5b3d5
jhuntoo <jonnolee84@gmai.com>
2017-06-26 14:14:02 +0100

    install_helm.sh

---
+++ scripts/install_helm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+HELMVERSION=v2.5.0
+PLATFORM=`uname | tr '[:upper:]' '[:lower:]'`
+HELM_TAR_FILE="helm-${HELMVERSION}-$PLATFORM-amd64.tar.gz"
+
+if [ -f ./.bin/helm ]
+then
+  echo 'skipping download of helm'
+else
+  curl -sSL "http://storage.googleapis.com/kubernetes-helm/${HELM_TAR_FILE}" -o ./.bin/${HELM_TAR_FILE} &&
+  tar zxvf ./.bin/${HELM_TAR_FILE}  -C ./.bin/ --strip-components 1 $PLATFORM-amd64/helm  &&
+  rm ./.bin/${HELM_TAR_FILE}
+fi
+
+
--- scripts/install_kops.sh
+++ scripts/install_kops.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 KOPS_VERSION=1.6.2
 PLATFORM=`uname | tr '[:upper:]' '[:lower:]'`
 
 mkdir -p .bin
-[ -f ./.bin/kops ] && echo 'skipping download of kops' || /usr/bin/curl -L -o ./.bin/kops https://github.com/kubernetes/kops/releases/download/$KOPS_VERSION/kops-$PLATFORM-amd64
+[ -f ./.bin/kops ] && echo 'skipping download of kops' || curl -L -o ./.bin/kops https://github.com/kubernetes/kops/releases/download/$KOPS_VERSION/kops-$PLATFORM-amd64
 chmod +x ./.bin/kops
